bceee56c4d06c7dd3ee87a98165dce33e2582957
Ronald Rink <ronald.rink@d-fens.net>
2015-07-30 11:47:21 +0200

    setup project with tests, added JustMock

---
+++ src/biz.dfch.CS.Annotations.CustomValidation.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace biz.dfch.CS.Annotations.CustomValidation.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [WorkItem(0)]
+        public void TestMethod1()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}

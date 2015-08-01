5c76156464390230014944a83d5c7e04c4be6ca5
Ronald Rink <ronald.rink@d-fens.net>
2015-08-01 16:52:40 +0200

    add custom validation for lists

---
+++ src/biz.dfch.CS.Annotations.CustomValidation.Tests/ListValidation.cs
@@ -0,0 +1,49 @@
+﻿/**
+ *
+ *
+ * Copyright 2015 Ronald Rink, d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace biz.dfch.CS.Annotations.CustomValidation.Tests
+{
+    public class ListValidation
+    {
+        [Required]
+        public List<string> RequiredListProperty { get; set; }
+
+        public List<string> OptionalListProperty { get; set; }
+
+        [CustomValidation(typeof(ListValidation), "CustomListPropertyValidator")]
+        public List<string> CustomListProperty { get; set; }
+
+        public static ValidationResult CustomListPropertyValidator(List<string> list, ValidationContext validationContext)
+        {
+            if(null == list || 0 >= list.Count)
+            {
+                return new ValidationResult(
+                    String.Format("{0}: CustomListPropertyValidator FAILED. List must contain at least 1 element.", validationContext.MemberName)
+                    ,
+                    new List<string>() { validationContext.MemberName }
+                    );  
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ src/biz.dfch.CS.Annotations.CustomValidation.Tests/ValidateListTest.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace biz.dfch.CS.Annotations.CustomValidation.Tests
+{
+    [TestClass]
+    public class ValidateListTest
+    {
+        [TestMethod]
+        public void ThisTestWillBeMarkedAsIgnored()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void RequiredEmptyListReturnsTrue()
+        {
+            var list = new ListValidation();
+            list.CustomListProperty = new List<string>( new string[] { "arbitrary string" });
+
+            // property under test
+            list.RequiredListProperty = new List<string>();
+
+            var validationResults = new List<ValidationResult>();
+            var validationContext = new ValidationContext(list);
+            var result = false;
+            result = Validator.TryValidateObject(list, validationContext, validationResults, true);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void RequiredOneItemListReturnsTrue()
+        {
+            var list = new ListValidation();
+            list.CustomListProperty = new List<string>(new string[] { "arbitrary string" });
+
+            // property under test
+            list.RequiredListProperty = new List<string>();
+            list.RequiredListProperty.Add("arbitrary string");
+
+            var validationResults = new List<ValidationResult>();
+            var validationContext = new ValidationContext(list);
+            var result = false;
+            result = Validator.TryValidateObject(list, validationContext, validationResults, true);
+            Assert.IsTrue(result);
+        }
+        
+        [TestMethod]
+        public void RequiredNullListReturnsValidationError()
+        {
+            var list = new ListValidation();
+            list.CustomListProperty = new List<string>(new string[] { "arbitrary string" });
+
+            // property under test
+            list.RequiredListProperty = null;
+
+            var validationResults = new List<ValidationResult>();
+            var validationContext = new System.ComponentModel.DataAnnotations.ValidationContext(list);
+            var result = false;
+            result = Validator.TryValidateObject(list, validationContext, validationResults, true);
+            Assert.IsFalse(result);
+            Assert.AreEqual(1, validationResults.Count());
+            var validationResult = validationResults[0];
+            Assert.AreEqual(1, validationResult.MemberNames.Count());
+            var memberName = validationResult.MemberNames.First();
+            Assert.AreEqual("RequiredListProperty", memberName);
+            StringAssert.Contains(validationResult.ErrorMessage, "RequiredListProperty");
+        }
+
+        [TestMethod]
+        public void OptionalNullListReturnsTrue()
+        {
+            var list = new ListValidation();
+            list.RequiredListProperty = new List<string>();
+            list.CustomListProperty = new List<string>(new string[] { "arbitrary string" });
+
+            // property under test
+            list.OptionalListProperty = null;
+
+            var validationResults = new List<ValidationResult>();
+            var validationContext = new ValidationContext(list);
+            var result = false;
+            result = Validator.TryValidateObject(list, validationContext, validationResults, true);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void OptionalEmptyListReturnsTrue()
+        {
+            var list = new ListValidation();
+            list.RequiredListProperty = new List<string>();
+            list.CustomListProperty = new List<string>(new string[] { "arbitrary string" });
+
+            // property under test
+            list.OptionalListProperty = new List<string>();
+
+            var validationResults = new List<ValidationResult>();
+            var validationContext = new ValidationContext(list);
+            var result = false;
+            result = Validator.TryValidateObject(list, validationContext, validationResults, true);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void OptionalOneItemListReturnsTrue()
+        {
+            var list = new ListValidation();
+            list.RequiredListProperty = new List<string>();
+            list.CustomListProperty = new List<string>(new string[] { "arbitrary string" });
+
+            // property under test
+            list.OptionalListProperty = new List<string>();
+            list.OptionalListProperty.Add("arbitrary string");
+
+            var validationResults = new List<ValidationResult>();
+            var validationContext = new ValidationContext(list);
+            var result = false;
+            result = Validator.TryValidateObject(list, validationContext, validationResults, true);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CustomNullListReturnsValidationError()
+        {
+            var list = new ListValidation();
+            list.RequiredListProperty = new List<string>();
+
+            // property under test
+            list.CustomListProperty = null;
+
+            var validationResults = new List<ValidationResult>();
+            var validationContext = new ValidationContext(list);
+            var result = false;
+            result = Validator.TryValidateObject(list, validationContext, validationResults, true);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void CustomEmptyListReturnsValidationError()
+        {
+            var list = new ListValidation();
+            list.RequiredListProperty = new List<string>();
+
+            // property under test
+            list.CustomListProperty = new List<string>();
+
+            var validationResults = new List<ValidationResult>();
+            var validationContext = new ValidationContext(list);
+            var result = false;
+            result = Validator.TryValidateObject(list, validationContext, validationResults, true);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void CustomOneItemListReturnsTrue()
+        {
+            var list = new ListValidation();
+            list.RequiredListProperty = new List<string>();
+
+            // property under test
+            list.CustomListProperty = new List<string>();
+            list.CustomListProperty.Add("arbitrary string");
+
+            var validationResults = new List<ValidationResult>();
+            var validationContext = new ValidationContext(list);
+            var result = false;
+            result = Validator.TryValidateObject(list, validationContext, validationResults, true);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CustomThreeItemListReturnsTrue()
+        {
+            var list = new ListValidation();
+            list.RequiredListProperty = new List<string>();
+
+            // property under test
+            list.CustomListProperty = new List<string>();
+            list.CustomListProperty.Add("1 - arbitrary string");
+            list.CustomListProperty.Add("2 - arbitrary string");
+            list.CustomListProperty.Add("3 - arbitrary string");
+
+            var validationResults = new List<ValidationResult>();
+            var validationContext = new ValidationContext(list);
+            var result = false;
+            result = Validator.TryValidateObject(list, validationContext, validationResults, true);
+            Assert.IsTrue(result);
+        }
+    }
+}
